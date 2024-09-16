af713b6bdc18bd59b901c68f30ab09578bd1daae
Mohit <mohitsudhan12@gmail.com>
2024-09-16 14:58:01 +0530

    Teacher integration

---
+++ src/main/java/constants/staffing/StaffConstants.java
@@ -0,0 +1,43 @@
+package constants.staffing;
+
+public class StaffConstants {
+    public static String name="Kunthavai";
+    public static String dob="13-03-1998";
+    public static String idNumber="Kun198";
+    public static String email="Test198@gmail.com";
+    public static String password="Test123#";
+    public static String introduction="Holding Master Degree in Maths";
+    public static String photoFilePath = "\\src\\main\\java\\base\\photo.png";
+
+    //form2
+    public static String staffID="K12345";
+
+    //form3
+    public static String contactNo="12345678";
+    public static String address="Tnagar Chennai";
+    public static String postalCode="621601";
+
+    //form5
+    public static String  salary="4300";
+
+    //form6
+    public static String annualLeave="12";
+    public static String medicalLeave="30";
+    public static String otherLeave="7";
+    public static String carryForwardLeave="5";
+
+    //fprm7
+    public static String uen="W12345";
+    public static String employmentAdd="No 40, Pioneer Mall";
+    public static String jobTitle="Centre Admin";
+    public static String duties="Manage ad hoc duties";
+    public static String  training="For 3 months";
+    public static String allowance="1";
+    public static String contractPeriod="11";
+    public static String probation="3";
+    public static String paynow= "90123456";
+    public static String accountNumber="1223498764";
+    public static String terminationNotice="3";
+
+
+}
